30b572cd5a84a5b46e54ffb3646bdfb240c4693d
Danilo Rossi <contact@danilorossi.me>
2020-01-07 21:48:07 +0100

    Updates

---
+++ precache-manifest.07cf7ca6b1533ccf4bb66e6eea9afcd7.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "355cac9f8df2f2d621cb9d8cdd9caded",
+    "url": "/statflix/index.html"
+  },
+  {
+    "revision": "14289e7e718409c69629",
+    "url": "/statflix/static/css/2.e87e35d5.chunk.css"
+  },
+  {
+    "revision": "8d555e36a71c4513015b",
+    "url": "/statflix/static/css/main.2f4032db.chunk.css"
+  },
+  {
+    "revision": "14289e7e718409c69629",
+    "url": "/statflix/static/js/2.3cd1ec6e.chunk.js"
+  },
+  {
+    "revision": "8d555e36a71c4513015b",
+    "url": "/statflix/static/js/main.aec316ff.chunk.js"
+  },
+  {
+    "revision": "34beee005e28f42b79b8",
+    "url": "/statflix/static/js/runtime~main.bcb4c2e0.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/statflix/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/statflix/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/statflix/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/statflix/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/statflix/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "d6080f6ad51868ee35b6b60056dbfc33",
+    "url": "/statflix/static/media/drop_file.d6080f6a.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/statflix/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/statflix/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/statflix/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/statflix/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/statflix/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/statflix/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "c4c27b7f273a4df2ded66103f599ed56",
+    "url": "/statflix/static/media/netflix_activity_download.c4c27b7f.png"
+  },
+  {
+    "revision": "240e7f690e0bf526fa6de2889ba00697",
+    "url": "/statflix/static/media/netflix_activity_page.240e7f69.png"
+  },
+  {
+    "revision": "7fa8a41c197be48b3b8afb4c2ebaa820",
+    "url": "/statflix/static/media/netflix_file.7fa8a41c.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/statflix/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/statflix/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/statflix/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/statflix/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/statflix/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "6aa9ce9fe6541d0625bd1d30b656f6ac",
+    "url": "/statflix/static/media/statflix_logo.6aa9ce9f.png"
+  }
+]);
